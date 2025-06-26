0fbf70de8e1aef1c7fc002fc3de8408e748692eb
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2025-06-26 10:45:33 -0400

    set mask threshold for 20ch coil

--- boldqc/__init__.py
+++ boldqc/__init__.py
@@ -18,9 +18,15 @@ def get_mask_threshold(sidecar):
         js = json.load(fo)
     bits_stored = js.get('BitsStored', None)
     receive_coil = js.get('ReceiveCoilName', None)
+    # 20ch coil should use a mask threshold of 3000.0 regardless of the bits stored
+    if receive_coil in ['HeadNeck_20']:
+        logger.info(f'scan has "{bits_stored}" bits and receive coil "{receive_coil}", setting mask threshold to 3000.0')
+        return 3000.0
+    # 12-bit scans should use a mask threshold of 150.0
     if bits_stored == 12:
         logger.info(f'scan has "{bits_stored}" bits and receive coil "{receive_coil}", setting mask threshold to 150.0')
         return 150.0
+    # 16-bit scans should use a mask threshold of 1500.0 for 32ch coil and 3000.0 for 64ch coil
     if bits_stored == 16:
         if receive_coil in ['Head_32']:
             logger.info(f'scan has "{bits_stored}" bits and receive coil "{receive_coil}", setting mask threshold to 1500.0')
